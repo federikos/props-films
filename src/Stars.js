bac9cdd14270217cb297213bd9bbd351e6075f5a
federikos <federikos@bk.ru>
2019-10-20 13:55:49 +0300

    remove ternary expression from jsx

--- src/Stars.js
+++ src/Stars.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import Star from './Star';
 
 const Stars = ({count}) => {
+  if(count < 0 && count > 5) {
+    return null;
+  }
+
   return (
-    count > -1 && count < 6
-    &&
     <ul className="card-body-stars u-clearfix">
       {
         new Array(count).fill(0).map((n, i) => <Star key={i} />)
